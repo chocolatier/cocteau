77d069c3307e2243f96c4fd6b1c08b685a4cc229
chocolatier <chocolatier@users.noreply.github.com>
2019-02-20 09:41:37 +1100

    ardunio code start

---
+++ arduino/arduino.ino
@@ -0,0 +1,35 @@
+#define HIGH_SPEED = 200
+#define LOW_SPEED = 100
+
+int M1 = 4;     //Motor 1
+int S1 = 5;     //M1 Speed
+
+int M2 = 7;     //Motor 2
+int S2 = 6;     //M2 Speed
+
+void setup(void) 
+{ 
+  Serial.begin(9600);
+
+} 
+ 
+void loop(void) 
+{
+
+}
+
+void moveForward() {
+  digitalWrite(M1,HIGH);    
+  digitalWrite(M2,HIGH); 
+}
+
+void setSpeedHigh() {
+   analogWrite (S1,HIGH_SPEED);
+   analogWrite (S2,HIGH_SPEED); 
+ }
+
+
+void setSpeedLow() {
+   analogWrite (S1,LOW_SPEED);
+   analogWrite (S2,LOW_SPEED); 
+ }
