e1142ad137a5b4a22a7544c2efb962f0f0fce87f
chocolatier <chocolatier@users.noreply.github.com>
2019-02-21 02:09:25 +1100

    define orders

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,8 +3,79 @@
 
 #define HIGH_SPEED 1000
 #define LOW_SPEED 200
+
 #define IR A0 // IR Analog Pin
 #define MODEL 1080 // IR Sensor Model
+
+#define PANPIN 8
+#define TILTPIN 7
+
+
+// Based on https://github.com/araffin/arduino-robust-serial/blob/master/arduino-board/slave.cpp
+// See SERIALLICENSE for more details. 
+
+
+#define PANSERVO 0
+#define  TILTSERVO 1
+#define  STOP 2
+#define  EXPLORE 3
+
+void wait_for_bytes(int num_bytes, unsigned long timeout)
+{
+	unsigned long startTime = millis();
+	//Wait for incoming bytes or exit if timeout
+	while ((Serial.available() < num_bytes) && (millis() - startTime < timeout)){}
+}
+
+// NOTE : Serial.readBytes is SLOW
+// this one is much faster, but has no timeout
+void read_signed_bytes(int8_t* buffer, size_t n)
+{
+	size_t i = 0;
+	int c;
+	while (i < n)
+	{
+		c = Serial.read();
+		if (c < 0) break;
+		*buffer++ = (int8_t) c; // buffer[i] = (int8_t)c;
+		i++;
+	}
+}
+
+int16_t read_i16()
+{
+  int8_t buffer[2];
+	wait_for_bytes(2, 100); // Wait for 2 bytes with a timeout of 100 ms
+	read_signed_bytes(buffer, 2);
+  return (((int16_t) buffer[0]) & 0xff) | (((int16_t) buffer[1]) << 8 & 0xff00);
+}
+
+int read_order(){
+  return Serial.read();
+}
+
+void read_serial(){
+  if (Serial.available() > 0) {
+    int order_received = read_order();
+    
+    switch (order_received){
+      case PANSERVO: {
+        int angle = read_i16();
+        writePan(angle);
+        break;
+      }
+
+      case TILTSERVO: {
+        int angle = read_i16();
+        writeTilt(angle);
+        break;
+      }
+
+
+    }
+
+  }
+}
 
 SharpIR SharpIR(IR, MODEL);
 
@@ -27,8 +98,8 @@ int on_precipie;
 void setup(void) 
 { 
   Serial.begin(9600);
-  panServo.attach(8);
-  tiltServo.attach(7);
+  panServo.attach(PANPIN);
+  tiltServo.attach(TILTPIN);
 } 
 
 void loop(void) 
@@ -45,15 +116,15 @@ void loop(void)
   //    moveForward();      
   //    }
 
-  panServo.write(110);
-  tiltServo.write(30);
-
-  delay(1000);
-
-  panServo.write(90);
-  tiltServo.write(40);
-
-  delay(10000);
+//  panServo.write(110);
+//  tiltServo.write(30);
+//
+//  delay(1000);
+//
+//  panServo.write(90);
+//  tiltServo.write(40);
+//
+//  delay(10000);
 
 }
 
