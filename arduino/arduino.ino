d214326d5c38d0a1b3b9bd9c8ea1df278b26da9d
chocolatier <chocolatier@users.noreply.github.com>
2019-02-20 19:54:21 +1100

    distance sensing

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,12 @@
-#define HIGH_SPEED 200
-#define LOW_SPEED 100
+#include <SharpIR.h>
+
+#define HIGH_SPEED 1000
+#define LOW_SPEED 200
+#define IR A0 // IR Analog Pin
+#define MODEL 1080 // IR Sensor Model
+
+
+SharpIR SharpIR(IR, MODEL);
 
 int M1 = 4;     //Motor 1
 int S1 = 5;     //M1 Speed
@@ -7,16 +14,30 @@ int S1 = 5;     //M1 Speed
 int M2 = 7;     //Motor 2
 int S2 = 6;     //M2 Speed
 
+int dist; // The Distance sensed by the IR Sensor
+
 void setup(void) 
 { 
   Serial.begin(9600);
+
+  dist = SharpIR.distance();  // this returns the distance to the object you're measuring
+
 
 } 
  
 void loop(void) 
 {
-//  setSpeedHigh();
-//  moveForward();
+  
+  dist = SharpIR.distance();  
+
+  if (dist < 15){
+    stop();
+    
+    } else {
+    setSpeedHigh();
+    moveForward();      
+    }
+
 }
 
 void moveForward() {
@@ -29,6 +50,11 @@ void setSpeedHigh() {
    analogWrite (S2,HIGH_SPEED); 
  }
 
+
+void stop() {
+   analogWrite (S1,0);
+   analogWrite (S2,0); 
+}
 
 void setSpeedLow() {
    analogWrite (S1,LOW_SPEED);
